3b51bfa52f70337f5ba472a5bd245b15538fe0ce
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-04-04 17:12:02 -0400

    feat: add routes

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 /* Configuration */
 dotenv.config();
@@ -16,3 +20,9 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cors());
+
+/* Routes */
+app.use("/client", clientRoutes); //all routes relevant to the client pages
+app.use("/general", generalRoutes); //user and dashboard
+app.use("/management", managementRoutes); //all routes relevant to the management pages
+app.use("/sales", salesRoutes); //all routes relevant to the sales pages
