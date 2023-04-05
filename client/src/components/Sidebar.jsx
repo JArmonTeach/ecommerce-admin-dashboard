0e8f64896856588fafd402dd040d9d94a73368ab
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-04-05 12:30:45 -0400

    feat: add list library for sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -33,6 +33,65 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import FlexBetween from './FlexBetween';
 import profileImage from "assets/sample-img.jpg";
 
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <HomeOutlined />
+    },
+    {
+        text: "Client Facing",
+        icon: null
+    },
+    {
+        text: "Products",
+        icon: <ShoppingCartOutlined />
+    },
+    {
+        text: "Customers",
+        icon: <Groups2Outlined />
+    },
+    {
+        text: "Transactions",
+        icon: <ReceiptLongOutlined />
+    },
+    {
+        text: "Geography",
+        icon: <PublicOutlined />
+    },
+    {
+        text: "Sales",
+        icon: null
+    },
+    {
+        text: "Overview",
+        icon: <PointOfSaleOutlined />
+    },
+    {
+        text: "Daily",
+        icon: <TodayOutlined />
+    },
+    {
+        text: "Monthly",
+        icon: <CalendarMonthOutlined />
+    },
+    {
+        text: "Breakdown",
+        icon: <PieChartOutlined />
+    },
+    {
+        text: "Management",
+        icon: null
+    },
+    {
+        text: "Admin",
+        icon: <AdminPanelSettingsOutlined />
+    },
+    {
+        text: "Performance",
+        icon: <TrendingUpOutlined />
+    },
+]
+
 const Sidebar = ({
     drawerWidth,
     isSidebarOpen,
@@ -51,7 +110,44 @@ const Sidebar = ({
 
     return (
         <Box component="nav">
+            {isSidebarOpen && (
+                <Drawer
+                    open={isSidebarOpen}
+                    onClose={() => setIsSidebarOpen(false)}
+                    variant="persistent"
+                    anchor="left"
+                    sx={{
+                        width: drawerWidth,
+                        "& .MuiDrawer-paper": {
+                            color: theme.palette.secondary[200],
+                            backgroundColor: theme.palette.background.alt,
+                            boxSizing: "border-box",
+                            borderWidth: isNonMobile ? 0 : "2px", 
+                            width: drawerWidth
+                        }
+                    }}
+                >
+                    <Box width="100%">
+                        <Box m="1.5rem 2rem 2rem 3 rem">
+                            <FlexBetween color={theme.palette.secondary.main}>
+                                <Box display="flex" alignItems="center" gap="0.5rem">
+                                    <Typography variant="h4" fontWeight="bold">
+                                        ECOMVISION
+                                    </Typography>
+                                </Box>
+                                {!isNonMobile && (
+                                    <IconButton onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+                                        <ChevronLeft />
+                                    </IconButton>
+                                )}
+                            </FlexBetween>
+                        </Box>
+                        <List>
 
+                        </List>
+                    </Box>
+                </Drawer>
+            )}
         </Box>
     );
 };
