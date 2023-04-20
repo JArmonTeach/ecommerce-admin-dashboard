cf5445aac6bda3b2442b7b56fa10608ef14a7ce9
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-04-19 21:32:10 -0400

    feat: finish client controller and add comments

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -7,16 +7,18 @@ export const getProducts = async (req, res) => {
 
         //cycles through all products, and for each product finds product stat using that product's id, then returns array object that has the all product information and stat information
         const productsWithStats = await Promise.all(
-            products.map(async (product) => {
+            products.map(async (product) => { //an aregate function could make this mapping quicker
                 const stat = await ProductStat.find({
-                    productId: product._id
+                    productId: product._id //this would be matching the foreign key
                 })
                 return{
                     ...product._doc,
                     stat,
                 }
             })
-        )
+        );
+
+        res.status(200).json(productsWithStats);
     } catch (error) {
         res.status(404).json({ mesage: error.message })
     }
