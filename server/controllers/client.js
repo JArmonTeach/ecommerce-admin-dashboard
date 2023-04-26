da97b4d2723840923502c1965142696e8a4563b6
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-04-26 16:30:25 -0400

    feat: add transactions controller

--- server/controllers/client.js
+++ server/controllers/client.js
@@ -1,6 +1,7 @@
 import Product from "../models/Product.js";
 import ProductStat from "../models/ProductStat.js";
 import User from "../models/User.js";
+import Transaction from "../models/Transaction.js";
 
 export const getProducts = async (req, res) => {
     try {
@@ -25,6 +26,7 @@ export const getProducts = async (req, res) => {
     }
 };
 
+//customers page is client side pagination where the server sends all info to the frontend and any sorting happens on the frontend
 export const getCustomers = async (req, res) => {
     try {
         const customers = await User.find({ role: "user"}).select("-password"); //the model has the password so we remove it in the frontend
@@ -33,3 +35,46 @@ export const getCustomers = async (req, res) => {
         res.status(404).json({ mesage: error.message });
     }
 };
+
+//for transactions page, we use server side pagination, where the frontend is just sent the data it needs to initial pagination, then the frontend will send its request for sorting to the backend when needed in which the server will send the new data out again; we do this with transactions because of how big the data is compared to the customers page
+export const getTransaction = async (req, res) => {
+    try {
+        //sort should look like this from the material UI datagrib: { "field": "userId", "sort" : "desc"}
+        const { page = 1, pageSize = 20, sort = null, search = ""} = req.query; //grabbing from the frontend
+
+        //formatted sort should look like that MongoDB should be able to read: { userId: -1 }
+        const generateSort = () => {
+            const sortParsed = JSON.parse(sort);
+            const sortFormatted = {
+                [sortParsed.field]: sortParsed.sort = "asc" ? 1 : -1
+            };
+            return sortFormatted;
+        }
+        const sortFormatted = Boolean(sort) ? generateSort() : {};
+
+        //sends frontend all the transactions that exist
+        const transactions = await Transaction.find({
+            //or allows the user to search different fields
+            $or: [
+                { const: { $regex: new RegExp(search, "i")}}, //this allows us to search the cost field
+                { userId: { $regex: new RegExp(search, "i")}}, //this allows us to search the userId field
+            ],
+        })
+
+            .sort(sortFormatted)
+            .skip(page * pageSize)
+            .limit(pageSize);
+        
+        //sends total transactions that exist
+        const total = await Transaction.countDocuments({
+            name: { $regex: search, $options: "i"}
+        });
+
+        res.status(200).json({
+            transactions,
+            total,
+        });
+    } catch (error) {
+        res.status(404).json({ mesage: error.message });
+    }
+}
