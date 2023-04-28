846d71480f964ac292e953a1af6bc356445697e9
Armon Antolin <74826437+JArmonTeach@users.noreply.github.com>
2023-04-28 16:05:40 -0400

    docs: add comment for controller

--- server/controllers/sales.js
+++ server/controllers/sales.js
@@ -2,9 +2,9 @@ import OverallStat from "../models/OverallStat.js";
 
 export const getSales = async (req, res) => {
   try {
-    const overallStats = await OverallStat.find();
+    const overallStats = await OverallStat.find(); //this becomes an array
 
-    res.status(200).json(overallStats[0]);
+    res.status(200).json(overallStats[0]); //there is only one datapoint in the array, that's why we only send this
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
